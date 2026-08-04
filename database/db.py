2082081688d778c77a2ef8d6dbb8125e0fada567
Meghana <meghanaannaladasu12@gmail.com>
2026-08-04 20:03:17 +0530

    Add database/db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,23 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import Base
 
+DATABASE_URL = "sqlite:///C:/Users/megha/prompts.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=False
+)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
